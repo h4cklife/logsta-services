00fecbc16b99de8f863a33c78d448b787aa96fe9
= <=>
2019-10-16 20:30:42 -0400

    Update to instructions

--- logstafeed.py
+++ logstafeed.py
@@ -14,7 +14,7 @@ This script may run better split into 2 scripts, but I wanted them combined, so 
 Check the extras directory for the originals. (May need modified and may not be up to date)
 
 Usage:
-    1. python3 logstafeed_final.py
+    1. python3 logstafeed.py
     2. tail -F snort.log | logstalgia --sync
 
 Developed by: @h4cklife
